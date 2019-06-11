39e9541d2bbf5409e89dba91ec753aebbb93250c
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-11 20:46:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,16 @@
-def turn_count
+def turn_count(board)
+  count = 0
+  board.each { |item|
+    if item == "x"
+      count += 1
+    elsif item == "o"
+      count += 1
+    else
+      count
+    end
+  }
 end
 
 def current_player
-  
+
 end
