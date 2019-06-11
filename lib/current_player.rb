c255c9b8f38e3675ad59a3a31ccf9af2db560b53
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-11 20:48:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   count = 0
   board.each { |item|
-    if item == "X"
+    if item = "X"
       count += 1
-    elsif item == "O"
+    elsif item = "O"
       count += 1
     else
       count
